86470da7e0cee18e024041ad5668cb3f02047873
Reeshita Paul <77070824+reeshi-18@users.noreply.github.com>
2021-04-16 02:44:27 +0530

    create file

---
+++ automate.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Do you have an existing key or want to create a new one?"
+echo "0: Use existing key"
+echo "1: Create new key"
+read choice
+
+if[$choice -eq '0'];
+then 
+	gpg --list-secret-keys --keyid-format LONG
+	echo "enter the key ID"
+	read id
+elif [$choice -eq '1'];
+then 
+	gpg --full-generate-key
+	gpg --list-secret-keys --keyid-format LONG
+	echo "enter the key ID"
+	read id
+fi
+
+git config --global user.signingkey $id
+git config --global commit.gpgsign true
+echo "gpg key successfully added"
+
+gpg --armor --export $id
+echo "Please Add this key in your Github account"
